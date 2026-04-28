2a0ee667b6213add616ac6ecfab8af0fc081a1dd
Daste <stefankar1000@gmail.com>
2026-04-28 12:50:30 +0200

    Deduplicate nixpkgs search results

---
+++ util.py
@@ -0,0 +1,17 @@
+from collections.abc import Callable, Generator, Hashable, Iterable
+
+
+def deduplicate[T, Key: Hashable](
+    objs: Iterable[T],
+    key: Callable[[T], Key],
+) -> Generator[T]:
+    """Deduplicate an iterable of objects using a key function"""
+
+    seen = set[Key]()
+    for obj in objs:
+        k = key(obj)
+        if k in seen:
+            continue
+
+        seen.add(k)
+        yield obj
